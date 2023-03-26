157e110686feb8fc12ce4d73e957d97c75cb7fcd
Christopher-Deo <chris@christopherdeo.com>
2023-03-26 18:56:49 -0500

    completed workbook 2.2

---
+++ Biography.java
@@ -0,0 +1,31 @@
+public class Biography {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+
+        // name (text)
+        String name = "Joe Blow";
+        // age (whole number)
+        int age = 551;
+        // country (text)
+        String country = "Russia";
+        // sport (text)
+        String sport = "underwater basket weaving";
+        // hours (whole number)
+        int hours = 8;
+        // game (text)
+        String game = "basket weaver 2000";
+        // subject (text)
+        String subject = "History";
+        // grade (character)
+        char grade = 'A';
+     
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favorite sport is "  +sport+ ". I play for "+ hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " +game + ".");
+        System.out.println("In school, my favorite subject was "+ subject + ", I scored a "+ grade +".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
